d85fc915d6677da91cf78dbd5c98bf1baf9acd88
Yusuf Solomon <ysolomon298@gmail.com>
2026-06-06 21:53:43 +0100

    feat: now the ldr values control the LED, circuit and schematic diagrams have also been added

--- Auto_night_light_arduino/src/main.cpp
+++ Auto_night_light_arduino/src/main.cpp
@@ -1,18 +1,30 @@
 #include <Arduino.h>
 
 const int LDR_PIN = A0;
+const int LED_PIN = 9;
+
+int ldrValue = 0;
+int brightness = 0;
 
 void setup()
 {
     Serial.begin(115200);
+
+    pinMode(LED_PIN, OUTPUT);
 }
 
 void loop()
 {
-    int ldrValue = analogRead(LDR_PIN);
+    ldrValue = analogRead(LDR_PIN);
+
+    brightness = map(ldrValue, 0, 1023, 255, 0);
+    analogWrite(LED_PIN, brightness);
 
     Serial.print("LDR Value: ");
     Serial.println(ldrValue);
 
-    delay(200);
+    Serial.print("Brightness: ");
+    Serial.println(brightness);
+
+    delay(50);
 }
